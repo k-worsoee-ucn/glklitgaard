d1e21f6f9ba27bceb3bd45d8ee21b93e9d931997
k-worsoee-ucn <10461960@ucn.dk>
2024-05-29 13:00:09 +0200

    Horse archive complete

--- themes/glklitgaard/archive-heste.php
+++ themes/glklitgaard/archive-heste.php
@@ -15,6 +15,8 @@
     $hest = new WP_Query(array(
         'posts_per_page' => -1,
         'post_type' => 'Heste',
+        'orderby' => 'date',
+        'order' => 'ASC'
 
     ));
 
@@ -42,12 +44,19 @@
     <p class="hidden horseshoeurl"><?php echo get_theme_file_uri("/assets/images/horseshoetrail.png") ?></p>
 </div>
 <div class="bottom-cta col-span-12 py-32 -mt-5 z-20 grid grid-cols-12">
-    <div class="col-start-1 col-span-7 grid grid-cols-3">
-        <img src="<?php echo get_theme_file_uri("/assets/images/ctaimg1.png") ?>" alt="" class="bg-white pt-6 px-6 pb-40 w-8/12">
-        <img src="<?php echo get_theme_file_uri("/assets/images/ctaimg2.png") ?>" alt="" class="bg-white pt-6 px-6 pb-40 w-8/12">
-        <img src="<?php echo get_theme_file_uri("/assets/images/ctaimg3.png") ?>" alt="" class="bg-white pt-6 px-6 pb-40 w-8/12">
+    <div class="col-start-1 col-span-7 grid grid-cols-11">
+        <div class="pt-6 px-6 pb-10 bg-white w-10/12 row-start-1 row-span-1 col-start-2 col-span-4 -rotate-6 shadow-lg ">
+            <img src="<?php echo get_theme_file_uri("/assets/images/ctaimg1.png") ?>" alt="" class="w-full h-3/5 object-cover">
+        </div>
+        <div class="pt-6 px-6 pb-10 bg-white w-10/12 row-start-1 row-span-1 col-start-5 col-span-4 rotate-12 shadow-lg">
+            <img src="<?php echo get_theme_file_uri("/assets/images/ctaimg2.png") ?>" alt="" class="w-full h-3/5 object-cover">
+        </div>
+        <div class="pt-6 px-6 pb-10 bg-white w-10/12 row-start-1 row-span-1 col-start-8 col-span-4 shadow-lg -ml20 z-10">
+            <img src="<?php echo get_theme_file_uri("/assets/images/ctaimg3.png") ?>" alt="" class="w-full h-3/5 object-cover">
+        </div>
+        
     </div>
-    <div class="col-start-8 col-span-4 text-center">
+    <div class="col-start-8 col-span-4 text-center my-auto">
         <h2 class="text-4xl">Vil du vide mere?</h2>
         <p class="text-lg">Har du spørgsmål til vores hest eller ønsker du at booke en ridetur på en af dem, så du velkommen til at kontakte os.</p>
         <a href="#" class="button"><button class="bg-main-interaction-color px-10 py-2 my-4 rounded-full">Kontakt os</button></a>
