43367df4cb62e39086de40968c9c373d6829b9ed
k-worsoee-ucn <10461960@ucn.dk>
2024-06-05 20:45:42 +0200

    Im running out of commit titles

--- themes/glklitgaard/archive-faciliteter.php
+++ themes/glklitgaard/archive-faciliteter.php
@@ -62,21 +62,21 @@ if ($Faciliteter->have_posts()) {
         <div class="Waves absolute top-14 h-fit overflow-x-hidden w-full ease-linear duration-150 -z-10">
             <?php echo file_get_contents(get_theme_file_uri("/assets/svg/Wave.svg")); ?>
         </div>
-        <div class="w-full grid grid-cols-1 md:w-8/12 lg:6/12 mx-auto">
+        <div class="w-full grid grid-cols-1 lg:w-8/12 mx-auto">
             <h2 class="text-3xl text-center w-full my-5">Belejlige Faciliteter</h2>
             <?php while ($Faciliteter->have_posts()) {
                 $Faciliteter->the_post();
                 if ($BelejAmnt % 2 != 0) { ?>
-                    <div class="relative w-full flex flex-col lg:flex-row">
+                    <div class="relative col-span-1 grid grid-cols-1 belejFaci">
                         <div class="bg-slate-50 relative md:absolute p-5 md:pb-16 w-8/12 md:w-5/12 z-30 left-0 top-0 mx-auto md:mx-none">
                             <a class="overflow-hidden w-full md:w-full h-fit block bg-slate-900" href="<?php echo post_permalink(); ?>">
                                 <img class="hover:opacity-85 w-full hover:rotate-3 duration-300 ease-in-out object-cover object-center hover:scale-110 " src="<?php echo the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>">
                             </a>
                         </div>
-                        <div class="relative w-10/12 md:w-8/12 min-h-fit h-1/5 ml-0 md:ml-auto md:my-10">
+                        <div class="relative w-10/12 md:w-12/12 min-h-fit h-1/5 ml-0 md:my-10 lg:col-start-2">
                             <div class="w-full mx-auto grid grid-cols-1  bg-yellow-100 py-8 pl-10">
-                                <h3 class="text-2xl w-full md:w-8/12 mr-10 ml-auto"><?php echo get_the_title(); ?></h3>
-                                <div class=" w-full md:w-8/12 mr-10 ml-auto">
+                                <h3 class="text-2xl w-full md:w-8/12 mr-auto lg:ml-10 ml-0"><?php echo get_the_title(); ?></h3>
+                                <div class=" w-full md:w-8/12 mr-auto lg:ml-10 ml-0">
                                     <?php if (has_excerpt()) {
                                         echo get_the_excerpt();
                                     } else {
